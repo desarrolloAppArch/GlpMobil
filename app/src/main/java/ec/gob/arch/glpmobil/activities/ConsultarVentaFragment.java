6af7955e6382f6be820553c9949b73fa204df32b
gabrielamatute <soraya.matute@controlhidrocarburos.gob.ec>
2019-09-25 16:48:11 -0500

    Cambiar de usuario prueba 2

--- app/src/main/java/ec/gob/arch/glpmobil/activities/ConsultarVentaFragment.java
+++ app/src/main/java/ec/gob/arch/glpmobil/activities/ConsultarVentaFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,7 @@ public class ConsultarVentaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Log.i("log_glp_cupo ---->","INFO ConsultarVentaFragment --> onCreateView():" );
         return inflater.inflate(R.layout.fragment_consultar_venta, container, false);
     }
 
