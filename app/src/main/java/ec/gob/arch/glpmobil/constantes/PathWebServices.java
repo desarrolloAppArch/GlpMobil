bf66117b98836f6bc5d47d4015b4ea4038b07f56
Vponce-arch <55702443+Vponce-arch@users.noreply.github.com>
2019-12-23 16:58:35 -0500

    Insertar Persona y arreglo de interfaz historial

--- app/src/main/java/ec/gob/arch/glpmobil/constantes/PathWebServices.java
+++ app/src/main/java/ec/gob/arch/glpmobil/constantes/PathWebServices.java
@@ -15,7 +15,8 @@ public interface PathWebServices {
 	 *
 	 */
 
-	String PATH_BASE="http://172.16.15.0:8080/webServiceGlpMobilWeb/rest";
+	//String PATH_BASE="http://172.16.15.0:8080/webServiceGlpMobilWeb/rest";
+	String PATH_BASE="http://siscoh-pr2.controlhidrocarburos.gob.ec/webServiceGlpMobilWeb/rest";
 	String WS_USUARIO = "/ws-usuario";
 	String METODO_OBTENER_DISTRIBUIDORES="/metodo-obtenerDistribuidores";
 	String METODO_LOGIN_USUARIO="/metodo-loginUsuario";
